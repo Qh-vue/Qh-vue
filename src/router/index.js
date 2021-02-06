88e3a25c48539cd39aa9cc655caf881d85029e6f
fq <231744321@qq.com>
2021-02-06 16:02:12 +0800

    放弃layui

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import Router from 'vue-router'
 import Login from '@/components/login'
 import Register from '@/components/register'
 
-import '../../static/layui/layui.js'
-import '../../static/layui/css/layui.css'
+
 
 Vue.use(Router) 
 
