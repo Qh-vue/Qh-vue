0590246939bb83dd180de109f75be3b91ed90fb0
fq <231744321@qq.com>
2021-02-06 16:27:06 +0800

    2

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// import VueI18n from 'vue-i18n'
+import 'element-ui/lib/theme-chalk/index.css'
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 import router from './router'
 
 Vue.config.productionTip = false
+// Vue.use(VueI18n)
+Vue.use(Element)
 
-
+Vue.config.lang = 'zh-cn'
+Vue.locale('zh-cn', zhLocale)
+Vue.locale('en', enLocale)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
